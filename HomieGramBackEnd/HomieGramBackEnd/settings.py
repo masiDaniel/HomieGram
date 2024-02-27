423cb67f1bffa686b6e2fa9ebb8a55456b3b4fe5
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-02-27 07:08:35 +0300

    mod: added new installed apps

--- HomieGramBackEnd/HomieGramBackEnd/settings.py
+++ HomieGramBackEnd/HomieGramBackEnd/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # installed apps
+    'houses.apps.HousesConfig',
+    'comments.apps.CommentsConfig',
+
 ]
 
 MIDDLEWARE = [
